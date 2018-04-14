ccb7cd3e00a0b7cd74ef58294c6cbdb448dd8c53
tonkunnen <ton_kunnen@hotmail.com>
2018-04-14 21:18:38 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polynomial;
+
+/**
+ *
+ * @author ton_k
+ */
+public class Main {
+    public static void main(String[] args) {
+    Polynomial b = new Polynomial("3 3 4 4");
+        System.out.println(b);
+    }
+}
